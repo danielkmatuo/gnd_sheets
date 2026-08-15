a9a95bbf24d769a49f1597b8fe3c345d9b436c7b
Daniel Matuo <danielkmatuo@gmail.com>
2026-08-15 15:29:38 -0300

    better form at index.html. Starting to understand how to use js!

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,18 @@
-//changes data from index.html dynamically
 const classSelect = document.querySelector("#class");
 const classInfo = document.querySelector("#class-info");
 
+//make user able to only choose skills equal to the number of skills cap
+function quantitySkillsCheck (quantity, data) {
+    const skillIssues = data.skill_choices.choose
+    if (quantity > skillIssues) {
+        return false
+    }
+
+    return true
+}
+
+//changes data from index.html dynamically
+//TODO: add tools list in reference data
 classSelect.addEventListener("change", async function () {
     const selectedClass = classSelect.value;
 
@@ -21,16 +32,60 @@ classSelect.addEventListener("change", async function () {
 
     const data = await response.json();
 
-    classInfo.innerHTML = `
+    var html = `
         <h2>${data.name}</h2>
         <p>Hit Die: ${data.hit_die}</p>
         <p>Saving Throws: ${data.proficiencies.saving_throws.join(", ")}</p>
-        <p>Choose ${data.skill_choices.choose} skills:</p>
-        <p>${data.skill_choices.from.join(", ")}</p>
+        <p>Armor proficiencies: ${data.proficiencies.armor.join(", ")} </p>
+        <p>Weapon proficiencies: ${data.proficiencies.weapons.join(", ")}</p>
+        <p>Tools proficiencies: ${data.proficiencies.tools.join(", ")}</p>
+        <fieldset>
+            <legend>Choose ${data.skill_choices.choose} skills:</legend>
     `;
+
+    var numSkills = data.skill_choices.from.length
+    for (var i = 0; i < numSkills; i++) {
+        html += `
+        <div>
+            <input type="checkbox" id="skill${i}" name="skills" value="${data.skill_choices.from[i]}">
+            <label for="skill${i}">${data.skill_choices.from[i]}</label>
+        </div>
+        `;
+    }
+    
+    html += `
+    </fieldset>
+    `;
+
+    classInfo.innerHTML = html;
+
+    const skillCheckboxes = document.querySelectorAll(
+        'input[name="skills"]'
+    );
+
+    skillCheckboxes.forEach(function (checkbox) {
+        checkbox.addEventListener("change", function (event) {
+            const selectedSkills = document.querySelectorAll(
+                'input[name="skills"]:checked'
+            );
+
+            const quantity = selectedSkills.length;
+
+            const valid = quantitySkillsCheck(
+                quantity,
+                data
+            );
+
+            if (!valid) {
+                event.target.checked = false;
+
+                alert(`You can only choose ${maxSkills} skills.`);
+            }
+        });
+    });
 });
 
-//send form information to the go server
+//send form information to the go server and do the first validation of data
 const form = document.querySelector("#character-form");
 
 form.addEventListener("submit", async function(event) {
