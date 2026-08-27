66fb9d82dd91b4160ede3b22e974aaa83b6023da
Daniel Matuo <danielkmatuo@gmail.com>
2026-08-27 18:59:48 -0300

    wdym undefined object???

--- frontend/js/create.js
+++ frontend/js/create.js
@@ -1,12 +1,21 @@
 const classSelect = document.querySelector("#class");
 const classInfo = document.querySelector("#class-info");
-const characterStats = document.querySelector("#abilities");
+const characterStats = document.querySelectorAll("#abilities input");
 const form = document.querySelector("#character-form");
 const levelSelect = document.querySelector("#level");
-let creationBonusPoints = 0;
-let bonusPointsCap = 3;
 
+const characterStr = Number(document.querySelector("#str").value);
+const characterDex = Number(document.querySelector("#dex").value);
+const characterCon = Number(document.querySelector("#con").value);
+const characterInt = Number(document.querySelector("#int").value);
+const characterWis = Number(document.querySelector("#wis").value);
+const characterCha = Number(document.querySelector("#cha").value);
+
+let lastCharacterCost = 0;
+let creationBonusPoints = 0;
+let bonusPointsCap = 3; //TODO: change this baseline whenever I add feats options on character creation
 const bonusButtons = document.querySelectorAll("#abilities button");
+const resetBonusButton = document.querySelector("#reset-bonus");
 
 let allocatedBonus = {
     "str": 0,
@@ -17,16 +26,7 @@ let allocatedBonus = {
     "cha": 0
 }
 
-let currBonusState = {
-    "str": 0,
-    "dex": 0,
-    "con": 0,
-    "int": 0,
-    "wis": 0,
-    "cha": 0
-};
-
-let lastValidScores = {
+let validPointBuyState = {
     "str": 8,
     "dex": 8,
     "con": 8,
@@ -73,49 +73,38 @@ function instantiateCostsMap() {
     return scoresCosts;
 }
 
-function validateStatsSelection(scores, scoresCost) {
-    const currLevel = Number(levelSelect.value);
-    if (currLevel === 1) {
-        const maxCost = 27;
-        var cost = 0;
-        var isWithinRange = true;
-
-        for (i = 0; i < scores.length; i++) {
-            if (!(scores[i] >= 8 && scores[i] <= 15)) {
-                isWithinRange = false; 
-                return [true, cost, isWithinRange];
-            }
-
-            cost += scoresCost.get(scores[i]);
-        }
-
-        if (cost > maxCost) {
-            return [false, cost, isWithinRange];
-        }
-
-        return [true, cost, isWithinRange];
+function calculateTotalCost(attemptedState) {
+    const costMap = instantiateCostsMap();
+    let currCost = 0;
+   
+    for (const value of Object.values(attemptedState)) {
+        currCost += value; 
     }
-    else {
-        const isValid = validateStatsSelectionAboveLvlOne(scores);
 
-        if (!isValid) {
-            return [false, 0, isValid];
-        }
+    return currCost;
+}
 
-        return [false, 0, isValid];
+function validateAbilityCost() {
+    const characterCost = calculateTotalCost();
+
+    if (characterCost > 27 || characterCost < 0) {
+        alert("Invalid character. Total ability scores cost is above 27 or below 0");
+        return false;
     }
-    }
+
+    return true;
+}
 
 function validateCreationBonusPoints(bonus) {
     if (creationBonusPoints + bonus > bonusPointsCap) {
-        return false
+        return false;
     }
 
-    return true
+    return true;
 }
 
 //TODO: Find a way to make this code ensure the correct structure for the initial bonus points allocation (one stat +2 and another +1)
-function validateAllocationCreationBonusPoints(bonus) {
+function validateAllocationCreationBonusPoints(bonus, currStat) {
     const bonusKeys = [
         "str",
         "dex",
@@ -125,67 +114,29 @@ function validateAllocationCreationBonusPoints(bonus) {
         "cha"
     ];
 
+    let countOnes = 0;
 
     if (bonus > 2 || bonus < 0) {
         alert("Invalid bonus point allocation. Can only allocate +2 or +1");
         return false;
     }
 
-    let currDiff = 0;
+    let currStatBonus = 0;
+
+    if (allocatedBonus[currStat] > 2) {
+        alert("Invalid bonus point allocation. Same stat received both +1 and +2");
+        return false;
+    }
 
     for (let i = 0; i < bonusKeys.length; i++) {
-        currDiff += allocatedBonus[bonusKeys[i]] - currBonusState[bonusKeys[i]];
+        if (allocatedBonus[bonusKeys[i]] === 1) {
+            countOnes++;
+        }
     }
-    
-    if (currDiff > bonusPointsCap) {
-        alert("Invalid bonus point allocation. Trying to allocate bonus points beyond the budget");
+
+    if (countOnes > 1) {
+        alert("Invalid bonus point allocation. Character has more than one stat with a +1");
         return false;
-    }
-
-    let hasOneBonus = false;
-    let hasTwoBonus = false;
-
-    if (bonus === 2 && bonusPointsCap === 3) {
-        for (let i = 0; i < bonusKeys.length; i++) {
-            if (!hasOneBonus && allocatedBonus[bonusKeys[i]] === 1) {
-                hasOneBonus = true; 
-            }
-            else if (hasOneBonus && allocatedBonus[bonusKeys[i]] === 1) {
-                alert("Invalid bonus point allocation. Trying to allocate more than one +1 for the creation bonus points budget")
-                return false;
-            }
-        } 
-
-        if (hasOneBonus) {
-            return true; 
-        }
-        alert("something went wrong on the +2 branch of the algorithm, check again");
-        return false;
-    }
-    else if (bonus === 1 && bonusPointsCap === 3) {
-        for (let i = 0; i < bonusKeys.length; i++) {
-            if (!hasTwoBonus && allocatedBonus[bonusKeys[i]] === 2) {
-                hasTwoBonus = true;
-            }
-            else if (hasTwoBonus && allocatedBonus[bonusKeys[i]] === 2) {
-                alert("Invalid bonus point allocation. trying to allocate more than one +2 for the creation bonus points budget");
-                return false;
-            }
-        }
-
-        if (hasTwoBonus) {
-            return true;
-        }
-        alert("something went wrong on the +1 branch of the algorithm, check again");
-        return false;
-    }
-}
-
-function validateStatsSelectionAboveLvlOne(scores) {
-    for (let i = 0; i < scores.length; i++) {
-        if (!(scores[i] >= 1 && scores[i] <= 30)) {
-            return false;
-        }
     }
 
     return true;
@@ -259,58 +210,42 @@ classSelect.addEventListener("change", async function () {
 });
 
 //validate character ability scores from frontend
-characterStats.addEventListener("change", async function(event){
-    if (event.target.value === "") {
-        event.target.value = lastValidScores[event.target.id];
-        return;
-    }
+//TODO: refactor to fit the new funcs to validate the character stats
+characterStats.forEach(function(statInput) {
+    statInput.addEventListener("change", async function(event){
+        if (event.target.value === "") {
+            event.target.value = validPointBuyState[event.target.id];
+            return;
+        }
 
-    const characterStr = Number(document.querySelector("#str").value);
-    const characterDex = Number(document.querySelector("#dex").value);
-    const characterCon = Number(document.querySelector("#con").value);
-    const characterInt = Number(document.querySelector("#int").value);
-    const characterWis = Number(document.querySelector("#wis").value);
-    const characterCha = Number(document.querySelector("#cha").value);
-    const characterLvl = Number(form.querySelector("#level").value);
+        let currAttempt = {
+            "str": characterStr,
+            "dex": characterDex,
+            "con": characterCon,
+            "int": characterInt,
+            "wis": characterWis,
+            "cha": characterCha
+        };
+        
+        currAttempt[event.target.id] = Number(event.target.value);
+        const currCost = calculateTotalCost(currAttempt);
+        const isValidCost = validateAbilityCost(currCost);
 
-    const scores = [characterStr, characterDex, characterCon, characterInt, characterWis, characterCha];
-    const costsMap = instantiateCostsMap();
-    
-    const [costOk, cost, isWithinRange] = validateStatsSelection(scores, costsMap)
-
-    if (costOk && !isWithinRange) {
-        alert(`Your current score (${event.target.value}) is out of range (range must be within 8 to 15).`);
-        return;
-    } 
-    else if ((!costOk && cost != 0) && isWithinRange) {
-        alert(`Your current cost (${cost}) is above 27.`);
-        return;
-    }
-    else if ((!costOk && cost === 0) && !isWithinRange) {
-        alert(`Your current score(${event.target.value}) is out of range (range must be within 1 to 30).`)
-        return;
-    }
-    else if ((!costOk && cost === 0) && isWithinRange) {
-        lastValidScores[event.target.id] = Number(event.target.value);
-        document.querySelector("#curr-cost").textContent = "Current cost: not applicable for characters above level 1.";
-        return;
-    }
-    else if (costOk && isWithinRange) {
-        lastValidScores[event.target.id] = Number(event.target.value);
-        document.querySelector("#curr-cost").textContent = "Current cost: " + cost;
-        return;
-    }
-    else {
-        alert("Something unexpected happened...")
-        return;
-    }
+        if (isValidCost) {
+            lastCharacterCost = currCost;
+            validPointBuyState = currAttempt;
+            document.querySelector("#curr-cost").textContent = "Current Cost: " + lastCharacterCost;
+        }
+    });
 });
 
-//TODO: find a way to make this event work like: user click this specific button, then add the respective bonus points
+//gigantic mess of code to control the buttons behaviour
 bonusButtons.forEach(function(button) {
     button.addEventListener("click", function(event) {
-        const regex = /^(one|two)-bonus-(str|dex|con|int|wis|cha)$/;
+        const regex = /^(one|two|up|down).*(str|dex|con|int|wis|cha)$/;
         const match = event.target.id.match(regex);
+
+        let currCost = 0;
 
         if (match[1] === "one") {
             const creationBonusPointsOk = validateCreationBonusPoints(1);
@@ -320,36 +255,17 @@ bonusButtons.forEach(function(button) {
             }
 
             creationBonusPoints++;
-            switch (match[2]) {
-                case "str":
-                    document.querySelector("#str").value++;
-                    break;
-                case "dex":
-                    document.querySelector("#dex").value++;
-                    break;
-                case "con":
-                    document.querySelector("#con").value++;
-                    break;
-                case "int":
-                    document.querySelector("#int").value++;
-                    break;
-                case "wis":
-                    document.querySelector("#wis").value++;
-                    break;
-                case "cha":
-                    document.querySelector("#cha").value++;
-                    break;
-            } 
+            document.querySelector(`#${match[2]}`).value++;
+
             allocatedBonus[match[2]]++;
-            const bonusAllocationOk = validateAllocationCreationBonusPoints(1);
+            const bonusAllocationOk = validateAllocationCreationBonusPoints(1, match[2]);
 
             if (!bonusAllocationOk) {
                 allocatedBonus[match[2]]--;
+                creationBonusPoints--;
+                document.querySelector(`#${match[2]}`).value--;
                 return; 
             } 
-            else {
-                currBonusState[match[2]]++;
-            }
         }
         else if (match[1] == "two") {
             const creationBonusPointsOk = validateCreationBonusPoints(2);
@@ -359,44 +275,67 @@ bonusButtons.forEach(function(button) {
             }
 
             creationBonusPoints += 2;
-            switch (match[2]) {
-                case "str":
-                    document.querySelector("#str").value = Number(document.querySelector("#str").value) + 2;
-                    break;
-                case "dex":
-                    document.querySelector("#dex").value = Number(document.querySelector("#dex").value) + 2;
-                    break;
-                case "con":
-                    document.querySelector("#con").value = Number(document.querySelector("#con").value) + 2;
-                    break;
-                case "int":
-                    document.querySelector("#int").value = Number(document.querySelector("#int").value) + 2;
-                    break;
-                case "wis":
-                    document.querySelector("#wis").value = Number(document.querySelector("#wis").value) + 2;
-                    break;
-                case "cha":
-                    document.querySelector("#cha").value = Number(document.querySelector("#cha").value) + 2;
-                    break;
-            } 
+            document.querySelector(`#${match[2]}`).value = Number(document.querySelector(`#${match[2]}`).value) + 2;
+
             allocatedBonus[match[2]] += 2;
-            const bonusAllocationOk = validateAllocationCreationBonusPoints(2);
+            const bonusAllocationOk = validateAllocationCreationBonusPoints(2, match[2]);
 
             if (!bonusAllocationOk) {
                 allocatedBonus[match[2]] -= 2;
+                creationBonusPoints -= 2;
+                document.querySelector(`#${match[2]}`).value = Number(document.querySelector(`#${match[2]}`).value) - 2;
                 return; 
-            } 
-            else {
-                currBonusState[match[2]] += 2;
             }
         }
+        else if (match[1] === "up") {
+            currAttempt = validPointBuyState;
+            currAttempt[match[2]]++;
+
+            currCost = calculateTotalCost(currAttempt)
+            const isValidCost = validateAbilityCost(currCost);
+
+            if (!isValidCost) {
+                alert("Invalid ability scores. Total cost above or below the budget")
+                return;
+            }
+
+            validPointBuyState = currAttempt;
+            document.querySelector(`#${match[2]}`).value++;
+            lastCharacterCost = currCost;
+        }
+        else if (match[1] === "down") {
+            currAttempt = validPointBuyState;
+            currAttempt[match[2]]--;
+
+            currCost = calculateTotalCost(currAttempt)
+            const isValidCost = validateAbilityCost(currCost);
+
+            if (!isValidCost) {
+                alert("Invalid ability scores. Total cost above or below the budget")
+                return;
+            }
+
+            validPointBuyState = currAttempt;
+            document.querySelector(`#${match[2]}`).value--;
+            lastCharacterCost = currCost;
+        }
         else {
-            alert("Couldnt find button");
+            alert("Couldn't find buttons");
             return;
         }
 
         document.querySelector("#curr-bonus").textContent = `Available Bonus Points: ${bonusPointsCap - creationBonusPoints}`;
     });
+});
+
+resetBonusButton.addEventListener("click", async function() {
+    creationBonusPoints = 0;
+    for (const key of Object.keys(allocatedBonus)) {
+        allocatedBonus[key] = 0;
+        document.querySelector(`#${key}`).value = lastValidScores[key]; 
+    }
+
+    document.querySelector("#curr-bonus").textContent = `Available Bonus Points: ${bonusPointsCap}`;
 });
 
 //send form information to the go server and do the first validation of data
